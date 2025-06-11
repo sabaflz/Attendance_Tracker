59faaf654146385f8295e5bcea730dea356a9cd0
sabaflz <sabaa.feilizadeh@gmail.com>
2025-06-11 12:01:37 -0700

    refactor: Change 'Total Unique Members' to 'Total Members' in attendance tables

--- attendance_processor.py
+++ attendance_processor.py
@@ -103,7 +103,7 @@ def process_attendance_data(base_dir: Path, report_type: str) -> Optional[pd.Dat
     df['Total'] = df.sum(axis=1)
     
     # Add total row (unique members per month)
-    df.loc['Total Unique Members'] = df.apply(lambda x: (x > 0).sum())
+    df.loc['Total Members'] = df.apply(lambda x: (x > 0).sum())
     
     # Sort by total attendance
     df = df.sort_values('Total', ascending=False)
